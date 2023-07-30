69588a8bb31180fd67f03bcd9657b43db712d4cd
aflahur <aflahurrofik@gmail.com>
2023-07-30 21:05:36 +0700

    cucumber parabank

---
+++ src/main/java/config/env_target.java
@@ -0,0 +1,9 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env_target {
+    protected WebDriver driver;
+    protected String baseUrl = "https://www.saucedemo.com/";
+    protected String Parabank = "https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC";
+}
